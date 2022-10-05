42d301b13236136fd36bcd19928ebbf3bbfc1ad5
Sam Warring <samwarring@gmail.com>
2022-10-04 22:19:37 -0700

    For fun - documenting `stdin_read_line`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-/// Read a line from standard input. If EOF, return None.
-/// The returned line is not trimmed. Panics if there is
-/// any error reading the line.
+//! Functions commonly used in AOC solutions.
+
+/// Reads an optional line from standard input.
+/// 
+/// The resulting line, if any, is not trimmed. On EOF, this
+/// function returns None.
+/// 
+/// # Panics
+/// 
+/// The function panics on any error reading from stdin.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use advent_of_code_2021::*;
+/// while let Some(line) = stdin_read_line() {
+///     print!("Read line: {}", line);
+/// }
+/// println!("End of standard input");
+/// ```
 pub fn stdin_read_line() -> Option<String> {
     let mut line = String::new();
     match std::io::stdin().read_line(&mut line) {
